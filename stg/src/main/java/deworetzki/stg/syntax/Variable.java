9e75907cbaaeadfab93750883e4f0f176d7ef985
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2021-11-24 21:34:42 +0100

    Added static method to generate an arbitrary amount of unused Variable instances.

--- stg/src/main/java/deworetzki/stg/syntax/Variable.java
+++ stg/src/main/java/deworetzki/stg/syntax/Variable.java
@@ -3,6 +3,10 @@ package deworetzki.stg.syntax;
 import deworetzki.parse.Position;
 import deworetzki.stg.visitor.Visitor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * A Variable is a name referring to a heap allocated closure.
  */
@@ -39,5 +43,21 @@ public final class Variable extends Atom {
             return this.name.equals(var.name);
         }
         return false;
+    }
+
+    public static List<Variable> arbitrary(int amount) {
+        List<Variable> arbitraryVariables = new ArrayList<>(amount);
+        for (int i = 0; i < amount; i++) {
+            arbitraryVariables.add(new Variable(freshName()));
+        }
+        return arbitraryVariables;
+    }
+
+    private static final AtomicInteger UNIQUE_VARIABLE_COUNTER = new AtomicInteger();
+    private static final char START = '\u03B1', END = '\u03C9';
+
+    private static String freshName() {
+        int id = UNIQUE_VARIABLE_COUNTER.getAndIncrement();
+        return String.format("%c%d", START + (id % (END - START)), id / (END - START));
     }
 }
