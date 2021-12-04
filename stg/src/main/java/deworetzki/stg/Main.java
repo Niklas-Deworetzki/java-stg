0ed3fbe873ef0c80676352452bac8a92b4b698e7
Niklas Deworetzki <niklas-deworetzki@t-online.de>
2021-12-04 22:43:20 +0100

    Added stub entry point to verify the grammar.

--- stg/src/main/java/deworetzki/stg/Main.java
+++ stg/src/main/java/deworetzki/stg/Main.java
@@ -1,4 +1,34 @@
 package deworetzki.stg;
 
-public class Main {
+import deworetzik.stg.parse.Parser;
+import deworetzki.parse.Source;
+import deworetzki.parse.symbol.RichSymbolFactory;
+import deworetzki.stg.parser.Scanner;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public final class Main {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            final File inputFile = new File(arg);
+            if (inputFile.exists()) {
+                final Source source = Source.fromFile(inputFile);
+                try (Reader input = new InputStreamReader(new FileInputStream(inputFile))) {
+                    final Scanner lexer = new Scanner(input, source);
+                    final Parser parser = new Parser(lexer, new RichSymbolFactory());
+
+                    parser.parse();
+
+                    System.out.println(arg + ": Success!");
+                } catch (Exception exception) {
+                    System.out.println(exception.getMessage());
+                }
+            }
+        }
+    }
 }
